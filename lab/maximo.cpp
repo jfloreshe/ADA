02f009507cfb9c3eb494a8a3f553af17f5a859f8
jfloreshe <pis@LAPTOP-UFGCPHN9.localdomain>
2020-06-17 17:22:33 -0500

    lab6

---
+++ lab/maximo.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <fstream>
+
+#define INF 20000
+#define TOTALTEST 200
+long* generate_array(const long);
+long max(long*, long, long&);
+void mostrar_array(long* , const long);
+double promedioMaxLinea4(long*);
+
+long Resultado[TOTALTEST];
+int main(){
+	std::ofstream xlstabla{"lab6.txt"};
+	const int base = 2;
+	int exptamano = 8;
+
+	srand (time(NULL));
+	
+	for(exptamano; exptamano<=20; exptamano++){
+		long size = 1;
+		for(int i=1;i<=exptamano;i++){
+			size *= base;
+		}
+		for(int totalTest=1;totalTest<=TOTALTEST;totalTest++){
+			long*A = generate_array(size);
+			long totalLinea4 = 0;
+			max(A,size,totalLinea4);
+			Resultado[totalTest-1] = totalLinea4;
+			delete[]A;
+			A = nullptr;
+		}
+		xlstabla << size<<'\t';
+		printf("%ld ",size);
+		double promedio = promedioMaxLinea4(Resultado);
+		xlstabla << promedio<<'\n';
+		std::cout << promedio << std::endl;	
+	}
+	xlstabla.close();
+}
+long* generate_array(const long size){
+	long *array = new long[size]; 
+	long i;
+	for(i=0;i<size; ++i){
+		array[i]= rand() % INF + 1;
+	}
+	return array;
+}
+double promedioMaxLinea4(long* A){
+	double promedio =0;
+	for(int i=0; i<TOTALTEST; i++){
+		promedio += A[i];
+	}
+	promedio /= (TOTALTEST*1.0f);
+	return promedio;
+}
+void mostrar_array(long* array, const long size){
+	for(int i=0;i<size; ++i)
+		std::cout<<array[i]<<' ';
+	std::cout<<std::endl;
+}
+
+long max(long*A, long size, long& totalLinea4){
+	long max = A[0];
+	for(long i=1;i<size;i++){
+		if(A[i]>max){
+			max = A[i];
+			totalLinea4++;
+		}
+	}
+	return max;
+}
