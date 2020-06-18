e1ae691db11fc8c03543c4caeb60275fe9f80532
jfloreshe <pis@LAPTOP-UFGCPHN9.localdomain>
2020-06-17 22:49:55 -0500

    lab6 rehecho

--- lab/maximo.cpp
+++ lab/maximo.cpp
@@ -3,17 +3,19 @@
 #include <time.h>
 #include <stdlib.h>
 #include <fstream>
-
-#define INF 20000
+#include <algorithm>
+#define INF 1000000
 #define TOTALTEST 200
 long* generate_array(const long);
+long* generate_array_diferentes(const long);
+bool is_in_array(long,long*,long );
 long max(long*, long, long&);
 void mostrar_array(long* , const long);
 double promedioMaxLinea4(long*);
 
 long Resultado[TOTALTEST];
 int main(){
-	std::ofstream xlstabla{"lab6.txt"};
+	std::ofstream xlstabla{"/mnt/c/Users/User/Desktop/lab6.txt"};
 	const int base = 2;
 	int exptamano = 8;
 
@@ -25,7 +27,9 @@ int main(){
 			size *= base;
 		}
 		for(int totalTest=1;totalTest<=TOTALTEST;totalTest++){
-			long*A = generate_array(size);
+			long*A = generate_array_diferentes(size);
+//			mostrar_array(A,size);
+//			printf("done size %ld, test %d\n",size,totalTest);
 			long totalLinea4 = 0;
 			max(A,size,totalLinea4);
 			Resultado[totalTest-1] = totalLinea4;
@@ -47,6 +51,34 @@ long* generate_array(const long size){
 		array[i]= rand() % INF + 1;
 	}
 	return array;
+}
+long* generate_array_diferentes(const long size){
+	long *array = new long[size]; 
+	long i;
+	for(i=0;i<size;++i){
+		array[i]=i+1;
+	}
+	int x=0, y=0, z=0, swaps=0, temp=0;
+	swaps = rand()%size +size;
+	while(x<swaps){
+		y=rand()%size;
+		temp = array[y];
+		z = rand()%size;
+		array[y] = array[z];
+		array[z] = temp;
+		++x;
+	}
+	return array;
+}
+bool is_in_array(long number,long* A,long total){
+	bool itIs = false;
+	for(int i=0; i<total;++i){
+		if(A[i] == number){
+			itIs = true;
+			break;
+		}
+	}
+	return itIs;
 }
 double promedioMaxLinea4(long* A){
 	double promedio =0;
